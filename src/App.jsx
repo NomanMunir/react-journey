9e38725e49d3c6c3d14c4683a132059022333b2f
Nauman Munir <mrnomanmunir@gmail.com>
2025-01-21 13:50:09 +0400

    - Devided componentes from App file to there own files in components folder.

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,9 @@
 import { CORE_CONCEPTS } from "./data.js";
-import conceptImg from "./assets/react-core-concepts.png"
+import Header from "./components/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
 
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const desc = reactDescriptions[genRandomInt(2)];
-  return (
-    <header>
-      <img src={conceptImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {desc} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept({ image, title, description }) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
-
-function App(){
+function App()
+{
   return (
     <div>
       <Header />
