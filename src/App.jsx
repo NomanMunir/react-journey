e24fba6b54487be1a5c294cae9889b2ec2483086
Nauman Munir <mrnomanmunir@gmail.com>
2025-01-21 15:25:10 +0400

    - Learn that we can pass function from parent component to child component as prop

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,33 @@ import TabButton from './components/TabButton.jsx';
 
 function App()
 {
+  function onSelectHandler()
+  {
+    console.log("hello world!");
+  }
+
   return (
     <div>
       <Header />
       <main>
-      <section id="core-concepts">
-        <h2>Core Concepts</h2>
-        <ul>
-          <CoreConcept {...CORE_CONCEPTS[0]} />
-          <CoreConcept {...CORE_CONCEPTS[1]} />
-          <CoreConcept {...CORE_CONCEPTS[2]} />
-          <CoreConcept {...CORE_CONCEPTS[3]} />
-        </ul>
-      </section>
-      <section id="examples">
-        <h2>Examples</h2>
-        <menu>
-          <TabButton>Components</TabButton>
-          <TabButton>JSX</TabButton>
-          <TabButton>Props</TabButton>
-          <TabButton>State</TabButton>
-        </menu>
-      </section>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={onSelectHandler}>Components</TabButton>
+            <TabButton onSelect={onSelectHandler}>JSX</TabButton>
+            <TabButton onSelect={onSelectHandler}>Props</TabButton>
+            <TabButton onSelect={onSelectHandler}>State</TabButton>
+          </menu>
+        </section>
       </main>
     </div>
   );
