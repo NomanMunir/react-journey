afdfa48554ef6f9ed1ed13ad398402d16a3374c6
Nauman Munir <mrnomanmunir@gmail.com>
2025-02-04 15:24:56 +0400

    Learned how to use default props value. We can set the value in props as {ButtonsContainer = 'menu'} so we don't have to pass values in each component

--- React Core/src/components/Examples.jsx
+++ React Core/src/components/Examples.jsx
@@ -34,7 +34,6 @@ export default function Examples() {
     <Section id="examples">
       <h2>Examples</h2>
       <Tabs
-        ButtonsContainer="menu"
         buttons={
           <>
             <TabButton
