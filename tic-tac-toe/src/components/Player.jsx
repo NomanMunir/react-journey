fabf4b1e556c4c7c4bf5627525fe98e6b58d0cb5
Nauman Munir <mrnomanmunir@gmail.com>
2025-02-04 23:20:49 +0400

    Learned about updating state based on old state correctly. And did two way binding.

---
+++ tic-tac-toe/src/components/Player.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+
+export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(name);
+
+  function handleEditing() {
+    setIsEditing(editing => !editing);
+  }
+
+  function handleChange(e)
+  {
+    setPlayerName(e.target.value);
+  }
+  return (
+    <li>
+      <span className="player">
+        {
+          isEditing ?
+            (
+            <input
+              value={playerName}
+              onChange={handleChange}
+              type="text" required />
+            )
+            : (<span className="player-name">{playerName}</span>)
+        }
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditing}>{isEditing == false ? "Edit" : "Save"}</button>
+    </li>
+  );
+}
