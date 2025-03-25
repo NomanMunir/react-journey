b91725181916907e82fbe82a89b35cb423ddea22
Nauman Munir <mrnomanmunir@gmail.com>
2025-03-26 01:37:37 +0500

    - Added ResultModal component for a dialog.

---
+++ refs-and-portals/src/components/ResultModal.jsx
@@ -0,0 +1,11 @@
+export default ResultModal({ result, targetTime })
+{
+    return (<dialog className="result-modal">
+        <h2>You {result}</h2>
+        <p>The target time was <strong>{targetTime} seconds.</strong></p>
+        <p>You stop the timer with <strong>X</strong> seconds left.</p>
+        <form method="dialog">
+            <button>Close</button>
+        </form>
+    </dialog>);
+}
