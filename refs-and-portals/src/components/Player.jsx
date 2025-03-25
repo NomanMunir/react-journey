73fab74e2673404817f8d9dabc07b3a0c8f1a46b
Nauman Munir <mrnomanmunir@gmail.com>
2025-03-26 00:42:59 +0500

    - Learn about refs and how to use them. - refs can be bind to an element like input and then you can access the whole element using ref. - Refs does not work like state. They will not rerender an element like state.

--- refs-and-portals/src/components/Player.jsx
+++ refs-and-portals/src/components/Player.jsx
@@ -1,10 +1,22 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const [inputName, setInputName] = useState(null);
+  const playerName = useRef();
+
+  function handleInput()
+  {
+    setInputName(playerName.current.value);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome { inputName ?? "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text"
+          ref={playerName}
+        />
+        <button onClick={handleInput}>Set Name</button>
       </p>
     </section>
   );
