bcd3767b9265f1792bb9de224c42ece91c6fd72e
Nauman Munir <mrnomanmunir@gmail.com>
2025-05-05 12:45:19 +0400

    - Learned about useNavigation hook in react-router-dom.

--- 22-spa-with-adv-routing/frontend/src/pages/EventsLayout.js
+++ 22-spa-with-adv-routing/frontend/src/pages/EventsLayout.js
@@ -1,10 +1,12 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import EventsNavigation from "../components/EventsNavigation";
 
 export default function EventsLayout() {
+  const navigation = useNavigation();
   return (
     <>
       <EventsNavigation />
+      {navigation.state === "loading" && <p>Loading...</p>}
       <Outlet />
     </>
   );
