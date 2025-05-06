b7428e78945e3152e3b78ff7e272c0afdf8eb9b8
Nauman Munir <mrnomanmunir@gmail.com>
2025-05-06 17:05:26 +0400

    Learned about how to do authenticaiton in react-router

--- 23-react-authentication/frontend/src/pages/Authentication.js
+++ 23-react-authentication/frontend/src/pages/Authentication.js
@@ -1,7 +1,39 @@
-import AuthForm from '../components/AuthForm';
+import { redirect } from "react-router-dom";
+import AuthForm from "../components/AuthForm";
 
 function AuthenticationPage() {
   return <AuthForm />;
 }
 
 export default AuthenticationPage;
+
+export async function action({ request }) {
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get("mode") || "login";
+
+  const data = await request.formData();
+  const authData = {
+    email: data.get("email"),
+    password: data.get("password"),
+  };
+
+  console.log(authData, mode);
+  const response = await fetch("http://localhost:8080/" + mode, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify(authData),
+  });
+
+  if (response.status === 422 || response.status === 401) return response;
+
+  if (!response.ok) {
+    throw new Response(JSON.stringify({ message: "unknown error happened." }), {
+      status: 500,
+    });
+  }
+  const resData = await response.json();
+  localStorage.setItem("token", resData.token);
+  return redirect("/");
+}
