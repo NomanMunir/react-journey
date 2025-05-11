d93737563f56ebbe1b548fafcbd15a47d1bad4c7
Nauman Munir <mrnomanmunir@gmail.com>
2025-05-11 11:33:41 +0400

    - Added a loading.js file in  meals forlder. It will be used by nextjs as a loading component.

--- 26-intro-to-nextjs/nextjs-foodies-project/app/meals/page.js
+++ 26-intro-to-nextjs/nextjs-foodies-project/app/meals/page.js
@@ -1,15 +1,26 @@
 import Link from "next/link";
 
-export default function MealsPage() {
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+
+export default async function MealsPage() {
+  const meals = await getMeals();
   return (
-    <main>
-      <h2>Meals Page</h2>
-      <p>
-        <Link href="/meals/m1">Meal 1</Link>
-      </p>
-      <p>
-        <Link href="/meals/m2">Meal 2</Link>
-      </p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p> Choose your favorite recipe and cook it yourself. It is and fun.</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={meals} />
+      </main>
+    </>
   );
 }
--- 26-intro-to-nextjs/nextjs-foodies-project/components/main-header/main-header.js
+++ 26-intro-to-nextjs/nextjs-foodies-project/components/main-header/main-header.js
@@ -1,10 +1,15 @@
+"use client";
+
 import Link from "next/link";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import Image from "next/image";
 import MainheaderBackground from "./main-header-background";
+import { usePathname } from "next/navigation";
+import NavLink from "./nav-link";
 
 export default function MainHeader() {
+  const path = usePathname();
   return (
     <>
       <MainheaderBackground />
@@ -22,10 +27,10 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              <Link href="/community">Foodies Community</Link>
+              <NavLink href="/community">Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
