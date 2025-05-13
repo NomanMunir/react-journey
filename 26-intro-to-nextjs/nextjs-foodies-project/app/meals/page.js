aea6413580436d883b0b07eec24bb68af35dc021
Nauman Munir <mrnomanmunir@gmail.com>
2025-05-13 15:18:03 +0400

    - Learned about not-found page and error page. How to use them and where to use them.

--- 26-intro-to-nextjs/nextjs-foodies-project/app/meals/page.js
+++ 26-intro-to-nextjs/nextjs-foodies-project/app/meals/page.js
@@ -3,9 +3,13 @@ import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
 
-export default async function MealsPage() {
+async function Meals() {
   const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
+export default async function MealsPage() {
   return (
     <>
       <header className={classes.header}>
@@ -19,7 +23,11 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fethcing Meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
