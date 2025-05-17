a2f88ac4fff1ffc266a814965c88ceaa5a025f08
Nauman Munir <mrnomanmunir@gmail.com>
2025-05-17 12:44:57 +0400

    - Added meetups routes and components

---
+++ 26-intro-to-nextjs/nextjs-bigger-prject/components/meetups/MeetupDetail.js
@@ -0,0 +1,13 @@
+export default function MeetupDetail() {
+  return (
+    <>
+      <img
+        src="https://imgs.search.brave.com/nG1XXrjBGwj_rWKgiJkqEsDlf4PbjUpJ0kzu9eRx4Ag/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/aXN0b2NrcGhvdG8u/Y29tL3Jlc291cmNl/cy9pbWFnZXMvRnJl/ZVBob3Rvcy9GcmVl/LVBob3RvLTc0MHg0/OTItMTc0NDkxNTMz/My5qcGc"
+        alt="Some Meetup"
+      />
+      <h1>A first meetup</h1>
+      <address>Some street 5</address>
+      <p>The meetup description</p>
+    </>
+  );
+}
---
+++ 26-intro-to-nextjs/nextjs-bigger-prject/pages/index.js
@@ -0,0 +1,36 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First meetup.",
+    image:
+      "https://imgs.search.brave.com/nG1XXrjBGwj_rWKgiJkqEsDlf4PbjUpJ0kzu9eRx4Ag/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/aXN0b2NrcGhvdG8u/Y29tL3Jlc291cmNl/cy9pbWFnZXMvRnJl/ZVBob3Rvcy9GcmVl/LVBob3RvLTc0MHg0/OTItMTc0NDkxNTMz/My5qcGc",
+    address: "some address",
+    description: "This is a meetup.",
+  },
+  {
+    id: "m2",
+    title: "A Second meetup.",
+    image:
+      "https://imgs.search.brave.com/s7h4GwPFE3nOh8I-uZHXR22kIbzwafCF_Bye8A6B0io/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/aXN0b2NrcGhvdG8u/Y29tL3Jlc291cmNl/cy9pbWFnZXMvUGhv/dG9GVExQL0xpZmVz/dHlsZS0xMDMzNDU3/MjUwLmpwZw",
+    address: "some address",
+    description: "This is a meetup.",
+  },
+  {
+    id: "m3",
+    title: "A Third meetup.",
+    image:
+      "https://imgs.search.brave.com/rrKzTYbAUU2b4dswK28R2SzMUK8lCYIarNEv_gGAaBE/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvNTEz/NDM5MzQxL3Bob3Rv/L3BvcnRyYWl0LW9m/LWVudGh1c2lhc3Rp/Yy1idXNpbmVzcy1w/ZW9wbGUtaW4tY2ly/Y2xlLmpwZz9zPTYx/Mng2MTImdz0wJms9/MjAmYz1veHdzcThX/R0ZUMGl4bVNvam50/WUJFWnFpZm5lNFA3/RGxxT1diWENxV1Vr/PQ",
+    address: "some address",
+    description: "This is a meetup.",
+  },
+];
+
+export default function HomePage() {
+  return (
+    <>
+      <MeetupList meetups={DUMMY_MEETUPS} />
+    </>
+  );
+}
