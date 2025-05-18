3bc1e79b1c868e01bbe71ce54f3e286aac498597
Nauman Munir <mrnomanmunir@gmail.com>
2025-05-18 17:01:18 +0000

    - Learned about Debouncing. How to use it and when to use it.

--- 29-react-patterns/src/components/searchableList/SearchableList.jsx
+++ 29-react-patterns/src/components/searchableList/SearchableList.jsx
@@ -1,22 +1,28 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
-export default function SearchableList({ items, children }) {
+export default function SearchableList({ items, itemKeyFn, children }) {
   const [searchTerm, setSearchTerm] = useState("");
+
+  const lastChange = useRef();
 
   const searchResults = items.filter((item) =>
     JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase())
   );
 
   function handleChange(event) {
-    setSearchTerm(event.target.value);
+    if (lastChange) clearTimeout(lastChange.current);
+    lastChange = setTimeout(() => {
+      lastChange.current = null;
+      setSearchTerm(event.target.value);
+    }, 500);
   }
 
   return (
     <div className="searchable-list">
       <input type="search" placeholder="Search" onChange={handleChange} />
       <ul>
-        {searchResults.map((item, index) => (
-          <li key={index}>{children(item)}</li>
+        {searchResults.map((item) => (
+          <li key={itemKeyFn(item)}>{children(item)}</li>
         ))}
       </ul>
     </div>
