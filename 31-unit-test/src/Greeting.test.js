e53796e487909b44061d57d3fd25ea797cb4aec4
Nauman Munir <mrnomanmunir@gmail.com>
2025-05-20 08:14:44 +0000

    - Learn how to make a test suite.

--- 31-unit-test/src/Greeting.test.js
+++ 31-unit-test/src/Greeting.test.js
@@ -1,13 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./components/Greeting";
 
-test("renders hello world!", () => {
-  //Arrange
-  render(<Greeting />);
-  //Act
-  //..nothing
+describe("Greeting Component", () => {
+  test("renders hello world!", () => {
+    //Arrange
+    render(<Greeting />);
+    //Act
+    //..nothing
 
-  //Assert
-  const helloWorldElement = screen.getByText("Hello World!");
-  expect(helloWorldElement).toBeInTheDocument();
+    //Assert
+    const helloWorldElement = screen.getByText("Hello World!");
+    expect(helloWorldElement).toBeInTheDocument();
+  });
 });
