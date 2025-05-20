319506efe2c23c859220ed5fa6399ef58c8a7689
Nauman Munir <mrnomanmunir@gmail.com>
2025-05-20 10:22:31 +0000

    - Learn about how do declare complex type in type script like array or array of objects.

---
+++ 32-react-and-typescript/typescript-examples/basics.ts
@@ -0,0 +1,32 @@
+
+/// complex types:
+
+//array:
+
+let hobbies : string[];
+
+hobbies = ["hello", "world"];
+
+let person: {
+    name: string,
+    age: number
+};
+
+person = {
+    name: 'Max',
+    age: 32, 
+}
+
+let people: {
+    name: string,
+    age: number
+}[];
+
+people = [{
+    name: 'Max',
+    age: 32, 
+}, 
+{
+    name: 'Nauman',
+    age: 32, 
+}]
