8d7c0ed73a0d1009b85b9fcdd92aac3483fd2685
Nauman Munir <mrnomanmunir@gmail.com>
2025-05-20 11:01:19 +0000

    - Learned about generics. They are more like Templets from c++.

--- 32-react-and-typescript/typescript-examples/basics.ts
+++ 32-react-and-typescript/typescript-examples/basics.ts
@@ -30,3 +30,29 @@ people = [{
     name: 'Nauman',
     age: 32, 
 }]
+
+
+// union type and type inference
+
+let course : string| number = "complete react course";
+course = 12312;
+
+// function and type
+
+function add (a: number, b: number)
+{
+    return a+b;
+}
+
+
+// Generics
+
+function insertAtBeginning(array:any[], value:any)
+{
+    const newArray = [value, ...array]
+    return newArray;
+}
+
+const demoArr = [1, 2, 3];
+
+const updatedArr = insertAtBeginning(demoArr, 0);
